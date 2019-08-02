672bd8e0c17ea4e4866dce95d8f1851670abffc1
alexforsale <alexforsale@yahoo.com>
2019-08-02 23:12:06 +0700

    use emacs as $EDITOR, $VISUAL, and $ALTERNATE_EDITOR

--- common/.config/profile.d/90-defaults.sh
+++ common/.config/profile.d/90-defaults.sh
@@ -3,9 +3,9 @@
 # these variables can be override 
 
 # editors
-[ -z "${ALTERNATE_EDITORS}" ] && export ALTERNATE_EDITOR="nano"
-[ -z "${EDITOR}" ] && export EDITOR="vi"
-[ -z "${VISUAL}" ] && export VISUAL="vi"
+[ -z "${ALTERNATE_EDITORS}" ] && export ALTERNATE_EDITOR="emacsclient -t"
+[ -z "${EDITOR}" ] && export EDITOR="emacsclient -c"
+[ -z "${VISUAL}" ] && export VISUAL="emacsclient -c"
 
 # from https://www.gnupg.org/documentation/manuals/gnupg/Invoking-GPG_002dAGENT.html
 [ -z "${GPG_TTY}" ] && export GPG_TTY="$(tty)"
