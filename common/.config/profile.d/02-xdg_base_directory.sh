63f08524752a6da91dd177ba60d21cce5f39774c
alexforsale <alexforsale@yahoo.com>
2019-08-19 17:07:26 +0700

    profile.d: create XDG user directories if not exists
               ~/.config/user-dirs.dirs gets overwritten by
               /usr/bin/xdg-user-dirs-update, so it cannot be stowed.

--- common/.config/profile.d/02-xdg_base_directory.sh
+++ common/.config/profile.d/02-xdg_base_directory.sh
@@ -46,4 +46,20 @@ if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
     DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
 fi
 
+# XDG User Directories
+# https://wiki.archlinux.org/index.php/XDG_user_directories
+[ -z $XDG_DESKTOP_DIR ] && export XDG_DESKTOP_DIR="$HOME/Desktop"
+[ -z $XDG_DOWNLOAD_DIR ] && export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+[ -z $XDG_TEMPLATES_DIR ] && export XDG_TEMPLATES_DIR="$HOME/Templates"
+[ -z $XDG_PUBLICSHARE_DIR ] && export XDG_PUBLICSHARE_DIR="$HOME/Public"
+[ -z $XDG_DOCUMENTS_DIR ] && export XDG_DOCUMENTS_DIR="$HOME/Documents"
+[ -z $XDG_MUSIC_DIR ] && export XDG_MUSIC_DIR="$HOME/Music"
+[ -z $XDG_PICTURES_DIR ] && export XDG_PICTURES_DIR="$HOME/Pictures"
+[ -z $XDG_VIDEOS_DIR ] && export XDG_VIDEOS_DIR="$HOME/Videos"
+
+for dir in XDG_{DESKTOP,DOWNLOAD,TEMPLATES,PUBLICSHARE,DOCUMENT,MUSIC,PICTURES,VIDEOS}_DIR;do
+    [ ! -d $dir ] && mkdir -p $dir
+done
+unset dir
+
 export XDG_CONFIG_HOME XDG_CACHE_HOME XDG_DATA_HOME XDG_RUNTIME_DIR DBUS_SESSION_BUS_ADDRESS
