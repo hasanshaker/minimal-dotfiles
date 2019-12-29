99fa08b21588fe646fa693f4e6c380625d66aee2
alexforsale <alexforsale@yahoo.com>
2019-12-30 00:55:53 +0700

    fish: better way to source locale.conf

--- fish/.local/share/fish/local.d/04-locale.fish
+++ fish/.local/share/fish/local.d/04-locale.fish
@@ -2,5 +2,15 @@
 # ~/.local/share/fish/local.d/04-locale.fish
 # set locale if not defined
 
-[ -z "$LANG" ] && set --global --export LANG en_US.UTF-8
+if [ -z "$LANG" ]
+    if [ -n "$XDG_CONFIG_HOME"  -a -r "$XDG_CONFIG_HOME/locale.conf" ]
+        set --global --export LANG (gawk '/^LANG/{print substr($0,index($0,"=")+1)}' "$XDG_CONFIG_HOME/locale.conf")
+    else if [ -n "$HOME" -a -r "$HOME/.config/locale.conf" ]
+        set --global --export LANG (gawk '/^LANG/{print substr($0,index($0,"=")+1)}' "$HOME/.config/locale.conf")
+    else if [ -r /etc/locale.conf ]
+        set --global --export LANG (gawk '/^LANG/{print substr($0,index($0,"=")+1)}' "/etc/locale.conf")
+    end
+end
+set --query LANG; or set --global --export LANG "C"
+
 [ -z "$MM_CHARSET" ] && set --global --export MM_CHARSET en_US.UTF-8
