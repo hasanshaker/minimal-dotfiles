f05d2a1a38e73838c2f1a457fcadc3fe557e8ef6
alexforsale <alexforsale@yahoo.com>
2020-02-08 02:27:04 +0700

    common: fix variable name for packages.sh

--- common/.local/share/dotfiles/packages.sh
+++ common/.local/share/dotfiles/packages.sh
@@ -12,6 +12,6 @@ case ${DISTRO} in
             ncmpcpp alsa-utils maim socat unclutter unrar unzip xclip xdotool \
             xorg-xdpyinfo zathura zathura-pdf-mupdf poppler mediainfo atool highlight \
             pavucontrol byzanz transmission-cli ttf-font-awesome texlive-most"
-        aur-list="ts polybar transmission-remote-cli-git screenkey-git"
+        aur_list="ts polybar transmission-remote-cli-git screenkey-git"
         ;;
 esac
