c952cf103affef76b329a0ed2f01cab7da5e92e8
alexforsale <alexforsale@yahoo.com>
2020-02-08 02:32:55 +0700

    common:install-package.sh: possible conflicts

--- common/.local/share/dotfiles/install-package.sh
+++ common/.local/share/dotfiles/install-package.sh
@@ -15,6 +15,11 @@ case ${DISTRO} in
         if [ -z ${package_list} ];then
             . ~/.local/share/dotfiles/packages.sh
         fi
+        # possible conflicts
+        if [ $(pacman -Qq vim 2>/dev/null) ];then
+            printf "we'll replace vim with gvim\n"
+            sudo pacman -Rqq --noconfirm vim 2>/dev/null
+        fi
         sudo pacman -Sqq --noconfirm ${package_list}
         if ! $(command -v yay 2>/dev/null);then
             _install_yay
